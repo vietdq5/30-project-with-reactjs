89a1c4e44da391e6a09f20bd4b3a0613dc2a5d61
qvietd <qvietd@gmail.com>
2025-06-05 15:06:35 +0700

    add inti project 2

---
+++ 02_movie_application/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="app">
+      <nav className="navbar">Nav bar content</nav>
+      <main>Main Content</main>
+    </div>
+  )
+}
+
+export default App;
